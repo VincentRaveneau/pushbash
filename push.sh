e29b6eb90164a298aab08b6c0674d85b2217e83f
VincentRaveneau <vincent.raveneau@epitech.eu>
2021-09-25 10:00:16 +0200

    I pushed my file

--- push.sh
+++ push.sh
@@ -13,4 +13,13 @@ read -p 'Your commit message: ' commitMessage
 
 git commit -m "$commitMessage"
 
-read -p 'Are you sure that you wanna push ${file[*]}': response
+read -p "Are you sure that you wanna push ${file[*]}": response
+
+
+if [[ $response == "y" ]] || [[ $response == "Yes" ]] || [[$response == "yes"]]; then
+    git push
+    echo ${file[*]} "have been pushed"
+else
+    git rm --cache ${file[*]}
+    echo "Commit have been deleted. If you wanna push. Restart script with files that you want push in argument"
+    
